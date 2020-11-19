0dd6efd9f73872ed3188d863ca61bdee113f1c1d
LOURDES PIZARRO CASTRO <51811145+lpizarro2391@users.noreply.github.com>
2020-11-19 17:00:37 -0600

    maximo y mino de un numero

--- repaso_phtyon.py
+++ repaso_phtyon.py
@@ -1,1 +1,9 @@
+x= int(input("Ingrese el primer numero"))
+y=int(input("ingrese el segundo numero"))
 
+if x > y:
+    maximo = x
+else:
+    maximo = y
+
+print("el numero maximo entre", x, "y", y,"es",maximo)
