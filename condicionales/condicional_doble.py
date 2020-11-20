3688037a854603a2071946f556410734ca91af0b
LOURDES PIZARRO CASTRO <51811145+lpizarro2391@users.noreply.github.com>
2020-11-19 18:32:15 -0600

    correcion print primera condicion

--- condicionales/condicional_doble.py
+++ condicionales/condicional_doble.py
@@ -2,6 +2,7 @@ x= int(input("ingrese las horas trabajadas en la semana"))
 pago_hora=10
 if x<=40:
         Monto_pago_hora=pago_hora*x
+        print("Total a cobrar:",str(Monto_pago_hora))
 else:
         sobretiempo=x-40
         total_cobro=pago_hora*x + 1.5*sobretiempo
